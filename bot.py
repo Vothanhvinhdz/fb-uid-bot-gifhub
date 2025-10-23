8266eecc3e74b7d22440f626e3d0d166e439b3cc
Vothanhvinhdz <128980250+Vothanhvinhdz@users.noreply.github.com>
2025-10-23 09:03:44 +0700

    Update bot.py with hardcoded API tokens
    Replaced environment variables for TELEGRAM_TOKEN and FB_TOKEN with hardcoded values.

--- bot.py
+++ bot.py
@@ -4,8 +4,8 @@ from aiohttp import ClientSession
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-FB_TOKEN = os.getenv("FB_TOKEN")
+TELEGRAM_TOKEN = os.getenv("7618142601:AAH4_zzpHyy7wjioC9QbBCtXcuNO-roKl8s"")
+FB_TOKEN = os.getenv("EAAGNO4a7r2wBPyLgahrnYRBnA4qQKZAlY5aofyumyBqHRhPZCwOzCSevSOiaaGpWCxZABbm9OMeYMghSZA4q3KPfnmcw396tQPGI9cTZAqF9feQn33HJtjj4QqGa3ZCiD7EXZCGZCgxbwPpWFvLCywzZCY74Gd9Aa8xOoWkphBvZAUFrUsap7GrcnOoOjfsWmYSHCJtwZDZD")
 CONCURRENCY = int(os.getenv("CONCURRENCY", "6"))
 REQUEST_TIMEOUT = int(os.getenv("REQUEST_TIMEOUT", "10"))
 CACHE_TTL = int(os.getenv("CACHE_TTL", "300"))
